2edae49d4fb4ebad1b7b99f407156ee54b44191d
reginapizza <reginaelyse@gmail.com>
2019-10-17 15:09:11 -0400

    Updated api url in config and auth api

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://sheltered-lowlands-54306.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
